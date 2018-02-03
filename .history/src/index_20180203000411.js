4a0eafed626a250381ddafec3ada72e736a9e118
sanal <sanalpanicker1987@gmail.com>
2018-02-03 00:19:28 -0800

    boiler plate code
    boiler plate code

---
+++ .history/src/index_20180203000411.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <div><h1>Hi How are you</h1></div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
---
+++ .history/src/index_20180203000434.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <div><h1>Hi how are you</h1></div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
